f8e6eb5de3c02a3d992585ba2a8711880a2ce0ed
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2022-01-08 08:46:21 +0300

    add operation

--- main.go
+++ main.go
@@ -3,7 +3,23 @@ package main
 import (
 	"github.com/therecipe/qt/widgets"
 	"os"
+	"fmt"
+	"strconv"
+	"reflect"
 )
+
+func operation(label *widgets.QLabel, input_text string){
+	res := label.Text()
+	res_int, err := strconv.Atoi(res)
+	add_integer, err := strconv.Atoi(input_text)
+	fmt.Println(res_int, add_integer)
+	res = strconv.Itoa(res_int + add_integer)
+	if err != nil{
+		fmt.Println("error")
+	}
+	fmt.Println(reflect.TypeOf(add_integer), reflect.TypeOf(res_int) )
+	label.SetText(res)
+}
 
 func main() {
 	// Create application
@@ -23,20 +39,29 @@ func main() {
 
 	// Create a line edit and add it to the layout
 	input := widgets.NewQLineEdit(nil)
+	//input.SetValidator(widgets.NewQRegExpValidator(widgets.NewQRegExp("[0-9]*"), &input));
 	input.SetPlaceholderText("1. write something")
-	layout.AddWidget(input, 0, 0)
+	layout.AddWidget(input, 1, 0)
 
 	// Create a button and add it to the layout
 	button := widgets.NewQPushButton2("2. click me", nil)
-	layout.AddWidget(button, 0, 0)
+	layout.AddWidget(button, 2, 0)
+	label := widgets.NewQLabel2("0", nil, 0)
+	layout.AddWidget(label, 0, 0)
 
 
 	// Connect event for button
 	button.ConnectClicked(func(checked bool) {
 		//widgets.QMessageBox_Information(nil, "OK", input.Text(),
 		//widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
-		label := widgets.NewQLabel2(input.Text(), nil, 0)
-		layout.AddWidget(label, 0, 0)
+		fmt.Println(label.Text() == "")
+			
+		if label.Text() == "0" {
+
+			label.SetText(input.Text())
+		}else{
+			operation(label, input.Text())
+		}	
 	})
 
 	// Set main widget as the central widget of the window
