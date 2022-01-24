20b87fba0325a707a7a6da505365caafd24f13f7
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2022-01-24 08:31:33 +0300

    some fixes to execute terminal/ add db logic but without runstuff

--- src/lib/notificator/notificator.go
+++ src/lib/notificator/notificator.go
@@ -3,16 +3,29 @@ package notif
 import (
 	"os/exec"
     "log"
+    "bytes"
 )
+
+const ShellToUse = "bash"
+
+func shellout(command string) (error, string, string) {
+    var stdout bytes.Buffer
+    var stderr bytes.Buffer
+    cmd := exec.Command(ShellToUse, "-c", command)
+    cmd.Stdout = &stdout
+    cmd.Stderr = &stderr
+    err := cmd.Run()
+    return err, stdout.String(), stderr.String()
+}
 
 
 func NOTIFY_ME(my_string ... string){
     var cmd *exec.Cmd
     if len(my_string) > 0 {
         prompt := my_string[0]
-        cmd = exec.Command(`notify-send  -t 0 "` + prompt + `"`)
+        shellout(prompt)
     }else{
-        cmd = exec.Command(`notify-send  -t 0 "Bringing down the system"`)
+        shellout(`notify-send  -t 0 "Bringing down the system"`)
     }
 
     err := cmd.Run()
