ed22d80271cda1b0797e5470a38dca871d3ecbe1
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2022-02-05 11:43:39 +0300

    proto timer on gorutine

--- gor.go
+++ gor.go
@@ -11,24 +11,27 @@ func init() {
     start = time.Now()
 }
 
-func getChars(cs chan string) {
-    begin := 1500
-    for i:= 0; i<begin; i++ {
-        minutes := (begin % 3600) / 60;
-        seconds := begin % 60;
-        timeString := fmt.Sprintf("%02d:%02d", minutes, seconds);
-        begin-=1;
-        time.Sleep(1 * time.Second)
-        cs <- timeString
-    }
+func getChars(cs chan string, begin int ) {
+    
+    minutes := (begin % 3600) / 60;
+    seconds := begin % 60;
+    timeString := fmt.Sprintf("%02d:%02d", minutes, seconds);
+    cs <- timeString
+
 }
 
 func main() {
     fmt.Println("main execution started at time", time.Since(start))
     my_chan := make(chan string)
-    go getChars(my_chan)
-    time.Sleep(10 * time.Second)
-    fmt.Println(<-my_chan)
+    begin := 1500
+    for i:= 0; i<begin; i++ {
+        go getChars(my_chan, begin)
+        begin-=1;
+        time.Sleep(1 * time.Second)
+        fmt.Println(<-my_chan)
+    }
+    
+    
 
     fmt.Println("\nmain execution stopped at time", time.Since(start))
 }
