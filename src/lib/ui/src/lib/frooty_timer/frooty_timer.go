383d387d50ca3fcfcc06365d67273d6bfeab57a0
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2022-03-01 11:50:18 +0700

    in process fix timer

--- src/lib/ui/src/lib/frooty_timer/frooty_timer.go
+++ src/lib/ui/src/lib/frooty_timer/frooty_timer.go
@@ -5,8 +5,9 @@ import (
     "time"
     "github.com/therecipe/qt/widgets"
   // notif "./src/lib/notificator"	
-   "sync"
+//   "sync"
    // "math"
+    "context"
 )
 
 func calculateLimitOfTime(beforeBegiTime int) int{
@@ -22,41 +23,45 @@ func getChars(cs chan string, begin int ) {
     return 
 }
 
-func workLoop(begin int, realBegin int, runnerChannel chan int, timeLabel *widgets.QLabel){
-    var wg sync.WaitGroup
+func workLoop(begin int, realBegin int, //runnerChannel chan int,
+     timeLabel *widgets.QLabel){
     my_chan := make(chan string)
-    defaultTime := begin
+    //defaultTime := begin
     for i:= 0; i<begin; i++{
-        select{
-        case value, ok := <-runnerChannel:
-            if ok {
-                if value > 0{
-                  // notif.NOTIFY_ME()
-                    fmt.Println("values is 0")
-                    wg.Wait()
-                    workLoop(begin, defaultTime, runnerChannel, timeLabel)
-                }
-            } else {
-                fmt.Println("Channel closed!")
-            }     
-        default:      
-            // if timebegin < 0{
-            //     wg.Wait()
+        // select{
+        // case value, ok := <-runnerChannel:
+        //     if ok {
+        //         if value < 0{
+        //           // notif.NOTIFY_ME()
+        //             fmt.Println("values is 0")
+        //         //    wg.Wait()
+        //            workLoop(begin, defaultTime, runnerChannel, timeLabel)
+        //         }
+        //     } else {
+        //         fmt.Println("Channel closed!")
+        //     }     
+        // default:      
+            // if realBegin == 0{
+            //     close(my_chan)
             //     return 
             // }
+            // fmt.Println(realBegin)
+            
+            
             go getChars(my_chan, begin)
-            wg.Add(1)
-            realBegin-=1
             timeLabel.SetText(<-my_chan) 
             time.Sleep(1 * time.Second)
-            begin-=1;        
-        }
+            begin-=1;
+            realBegin-=1      
+        
     }
 
 }
 
 func FROOTY_TIMER(timeBegin int, placeForUse *widgets.QLabel, runFlag chan int){
     begin := calculateLimitOfTime(timeBegin) 
-    fmt.Println(begin)
-    workLoop(begin, timeBegin, runFlag, placeForUse)    
+    fmt.Println(runFlag)
+    ctx, cancel := context.WithCancel(context.Background())
+    workLoop(timeBegin, begin,// runFlag,
+         placeForUse)    
 }
