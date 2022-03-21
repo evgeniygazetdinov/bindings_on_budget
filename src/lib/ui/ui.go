f71b48ce6e5bf5c787ce274488d6afb247a212ba
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2022-03-21 07:17:21 +0300

    add formating

--- src/lib/ui/ui.go
+++ src/lib/ui/ui.go
@@ -3,22 +3,19 @@ package ui
 import (
 	// "github.com/therecipe/qt/gui"
 	//"github.com/therecipe/qt/core"
-	"github.com/therecipe/qt/widgets"
+	"github.com/therecipe/qt/widgets"	
 	// cal "./src/lib/cal"
-	db "./src/lib/db"
 	timer "./src/lib/frooty_timer"
+	db "./src/lib/db"
 	"fmt"
 	"reflect"
 )
-
 const DURATION = 1200
-
-func update_gui(label *widgets.QLabel, startButton *widgets.QPushButton, chanelPush chan int) {
+func update_gui(label *widgets.QLabel, startButton *widgets.QPushButton, chanelPush chan int){
 	go timer.FROOTY_TIMER(DURATION, label, startButton, chanelPush)
-
 }
 
-func UI_SHIT(window *widgets.QMainWindow) {
+func UI_SHIT(window *widgets.QMainWindow){
 	window.SetWindowTitle("budget")
 	window.SetMinimumSize2(200, 200)
 
@@ -43,19 +40,20 @@ func UI_SHIT(window *widgets.QMainWindow) {
 	// Connect event for button
 	startButton := widgets.NewQPushButton2("start", nil)
 	layout.AddWidget(startButton, 2, 0)
-	// handler start
+	// handler start 
 	isStartPushed := make(chan int)
-	isStartPushed <- 0
+	isStartPushed <- 0 
 	fmt.Println(reflect.TypeOf(isStartPushed))
 	startButton.ConnectClicked(func(checked bool) {
 		update_gui(timeLabel, startButton, isStartPushed)
 	})
 
+
 	currentTask.SetText(db.GET_LAST_TASK())
 
 	addButton.ConnectClicked(func(checked bool) {
-		if input.Text() != " " && !db.TASK_EXIST_IN_DB(input.Text()) {
-			db.QUERY_INPLACE(fmt.Sprintf("insert into tasks(tasks_id, name) values((SELECT MAX(tasks_id)from tasks)+1,'%s');", input.Text()))
+		if input.Text() != " " && !db.TASK_EXIST_IN_DB(input.Text()){
+			db.QUERY_INPLACE(fmt.Sprintf("insert into tasks(tasks_id, name) values((SELECT MAX(tasks_id)from tasks)+1,'%s');",input.Text()))
 			currentTask.SetText(input.Text())
 		}
 	})
@@ -63,3 +61,6 @@ func UI_SHIT(window *widgets.QMainWindow) {
 	// Set main widget as the central widget of the window
 	window.SetCentralWidget(mainWidget)
 }
+
+
+
